95e73774c58699e3261c8afd5dabde56c90f0e59
Youssef <youssef24p@gmail.com>
2025-05-03 13:32:43 +0300

    finish user class

--- src/User.java
+++ src/User.java
@@ -1,12 +1,86 @@
 public abstract class User {
-    protected int userId;
-    protected String username;
-    protected String password;
-    protected String name;
-    protected String email;
-    protected String contactInfo;
+    private String userId;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String contactInfo;
 
-    abstract void login();
-    abstract void logout();
-    abstract void updateProfile();
+    public User() {}
+
+
+    public User(String username, String password, String name, String email, String contactInfo) {
+        this.userId = userId;
+        setUsername(email);
+        setPassword(password);
+        this.name = name;
+        setEmail(email);
+        this.contactInfo = contactInfo;
+    }
+
+    abstract String login();
+    abstract String logout();
+    abstract String updateProfile();
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        if (username.length() < 3) {
+            throw new IllegalArgumentException("Username must be at least 3 charcters long");
+        } else {
+            this.username = username;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password.length() < 6 ) {
+            throw new IllegalArgumentException("Password must be at least 6 characters long");
+        } else if (password.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("Password must contains at least one number");
+        } else {
+            this.password = password;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email.matches(".*@.*")) {
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException("Email is not valid");
+        }
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
 }
