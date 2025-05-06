652d4b94699f04a19f6adf3b7f9e5e4f28761b7b
Youssef <youssef24p@gmail.com>
2025-05-06 21:21:22 +0300

    update Enrollment

---
+++ src/Helper.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Helper {
+
+    public static boolean isValidDate(String dateStr, String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        try {
+            LocalDate.parse(dateStr, formatter);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+}
