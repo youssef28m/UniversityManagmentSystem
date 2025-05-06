a8edf65f03c19fd336e3ae32fd22bd52f45274c1
Youssef <youssef24p@gmail.com>
2025-05-06 18:27:13 +0300

    .

---
+++ src/Enrollment.java
@@ -0,0 +1,58 @@
+import java.time.LocalDate;
+
+public class Enrollment {
+    private String studentId;
+    private int courseId;
+    private String enrollmentDate;
+    private int grade;
+    private String status;
+
+    public Enrollment(String studentId, int courseId) {
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.enrollmentDate = LocalDate.now().toString();
+        this.status = "Enrolled";
+    }
+
+    public void assignGrade(String grade) {
+        // Method not implemented
+    }
+
+    public String getStatus() {
+        // Method not implemented
+        return null;
+    }
+
+    public void withdraw() {
+        // Method not implemented
+    }
+
+    // Getters and Setters
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getStatusValue() {
+        return status;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import users.*;
 
 
@@ -6,6 +9,24 @@ public class Main {
     public static void main(String[] args) {
         DatabaseManager db = new DatabaseManager();
         db.createTables();
+
+        ArrayList<List<Object>> allCourses = db.getAllCourses();
+
+        int coursesToShow = Math.min(2, allCourses.size());
+        for (int i = 0; i < coursesToShow; i++) {
+            List<Object> course = allCourses.get(i);
+            System.out.println("Course #" + (i+1) + ":");
+            System.out.println("ID: " + course.get(0));
+            System.out.println("Title: " + course.get(1));
+            System.out.println("Description: " + course.get(2));
+            System.out.println("Credit Hours: " + course.get(3));
+            System.out.println("Instructor ID: " + course.get(4));
+            System.out.println("Max Capacity: " + course.get(5));
+            System.out.println("Schedule: " + course.get(6));
+            System.out.println("-----------------------------------");
+        }
+
+
 
     }
 }
